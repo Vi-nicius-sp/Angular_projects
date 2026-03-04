c328a9078c24ce254fae8b7d76dacc6eae7a00fc
vinicius <viniciuspereiradearaujo24@gmail.com>
2026-03-03 22:14:00 -0300

    Usando ngIf para fazer quadrado aparecer e desaparecer

--- segundo_projeto/src/app/app.ts
+++ segundo_projeto/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { BindingsComponente } from "./bindings-componente/bindings-componente";
+import { SegundoComponente } from "./segundo-componente/segundo-componente";
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  imports: [ BindingsComponente],
+  imports: [ SegundoComponente], //BindingsComponente
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
